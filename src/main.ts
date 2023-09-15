2462f06e50f17a9208aad5bffb2101bfe7f7834a
flamitas <alexp2291997@gmail.com>
2023-09-15 03:02:16 -0400

    iniciando

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.enableCors();
   const PORT = process.env.PORT ?? 1234
   await app.listen(PORT);
   console.log('Server on port', PORT)
